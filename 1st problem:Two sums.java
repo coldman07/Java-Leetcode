ccbcba2c5fd14de54a44540dfab10a7d6ba3d49e
Vibhu <144217754+VibhuYadav2005@users.noreply.github.com>
2025-03-17 21:02:12 +0530

    Update and rename 1st problem:Two sums to 1st problem:Two sums.java

--- 1st problem:Two sums.java
+++ 1st problem:Two sums.java
@@ -1,4 +1,4 @@
-1. Two Sum
+/* 1. Two Sum
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -22,7 +22,7 @@ Input: nums = [3,3], target = 6
 Output: [0,1]
 
 Solution: 
-
+*/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
